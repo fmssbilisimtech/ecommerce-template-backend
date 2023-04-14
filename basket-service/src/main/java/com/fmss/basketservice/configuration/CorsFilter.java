20acd4c43f898101c3dbe692f194543e1beb772a
muhammed alagöz <EXT02D14692>
2023-04-14 13:59:42 +0300

    Change filter

--- basket-service/src/main/java/com/fmss/basketservice/configuration/CorsFilter.java
+++ basket-service/src/main/java/com/fmss/basketservice/configuration/CorsFilter.java
@@ -19,8 +19,7 @@ public class CorsFilter implements HandlerInterceptor {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
-        response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
+        response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         if ("OPTIONS".equals(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
