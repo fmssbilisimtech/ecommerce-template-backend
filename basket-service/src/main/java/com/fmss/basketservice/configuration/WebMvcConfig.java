5336b0ad14db399ad6ae0563346ba33d78a34e5b
muhammed alagöz <EXT02D14692>
2023-04-14 14:11:54 +0300

    Change filter

--- basket-service/src/main/java/com/fmss/basketservice/configuration/WebMvcConfig.java
+++ basket-service/src/main/java/com/fmss/basketservice/configuration/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.fmss.basketservice.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +15,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(corsFilter).addPathPatterns("/**");
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600);
   }
 }
