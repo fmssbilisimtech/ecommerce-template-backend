f3c16c54034f13fa3a8c8eaf67770d6ed3fe2dff
muhammed alagöz <EXT02D14692>
2023-04-14 13:48:09 +0300

    Change filter

--- basket-service/src/main/java/com/fmss/basketservice/configuration/CorsFilter.java
+++ basket-service/src/main/java/com/fmss/basketservice/configuration/CorsFilter.java
@@ -5,19 +5,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
 
 @Slf4j
 @Component
-public class CorsFilter implements Filter {
+public class CorsFilter implements HandlerInterceptor {
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        log.info("CorsFilter::method:{}::url:{}", request.getMethod(), request.getRequestURI());
-
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
@@ -25,8 +23,15 @@ public class CorsFilter implements Filter {
         response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
         if ("OPTIONS".equals(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
-        } else {
-            chain.doFilter(request, response);
         }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
     }
 }
