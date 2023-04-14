f45a0cd24d599b71311c57da7950fdc7286b43a1
muhammed alagöz <EXT02D14692>
2023-04-14 13:28:04 +0300

    add log

--- basket-service/src/main/java/com/fmss/basketservice/configuration/CorsFilter.java
+++ basket-service/src/main/java/com/fmss/basketservice/configuration/CorsFilter.java
@@ -3,16 +3,20 @@ package com.fmss.basketservice.configuration;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class CorsFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("CorsFilter::method:{}::url:{}", request.getMethod(), request.getRequestURI());
+
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
