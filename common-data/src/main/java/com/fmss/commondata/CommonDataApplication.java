d68a446bf9b0042e4e9323d1ed8404bc6c58b30c
Taner GENC <taner.genc@consultant.turkcell.com.tr>
2023-04-16 22:07:58 +0300

    refactor-logs

--- common-data/src/main/java/com/fmss/commondata/CommonDataApplication.java
+++ common-data/src/main/java/com/fmss/commondata/CommonDataApplication.java
@@ -1,12 +1,24 @@
 package com.fmss.commondata;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@Slf4j
 public class CommonDataApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommonDataApplication.class, args);
     }
 
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            log.info("CommonData service started");
+        };
+
+    }
 }
--- log-service/src/main/java/com/fmss/logservice/LogServiceApplication.java
+++ log-service/src/main/java/com/fmss/logservice/LogServiceApplication.java
@@ -1,15 +1,27 @@
 package com.fmss.logservice;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories("com.fmss.logservice.repository")
+@Slf4j
 public class LogServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LogServiceApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            log.info("log service started");
+        };
+
     }
 
 }
