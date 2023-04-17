03df62237e2f66d09e7c5733e47eb03d2e616bda
Esat Postaci <esat.postaci@consultant.turkcell.com.tr>
2023-04-17 16:26:22 +0300

    swagger authorize

--- order-service/src/main/java/com/fmss/orderservice/configuration/OpenApiConfigConfiguration.java
+++ order-service/src/main/java/com/fmss/orderservice/configuration/OpenApiConfigConfiguration.java
@@ -3,6 +3,7 @@ package com.fmss.orderservice.configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,12 +13,21 @@ public class OpenApiConfigConfiguration {
 
     @Bean
     public OpenAPI customOpenAPI(@Value("order-service") String description,
-                                 @Value("v1.0") String version) {
+            @Value("v1.0") String version) {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
-                .info(new Info()
-                        .title("Order Service")
-                        .version(version)
-                        .description(description)
-                        .license(new License().name("order service")));
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT"))
+                        .info(new Info()
+                                .title("Order Service")
+                                .version(version)
+                                .description(description)
+                                .license(new License().name("order service"))));
     }
 }
