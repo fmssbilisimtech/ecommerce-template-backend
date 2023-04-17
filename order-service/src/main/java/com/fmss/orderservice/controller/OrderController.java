78a77c16c1b41a44df8891e920dcd18bbd5b32f4
Esat Postaci <esat.postaci@consultant.turkcell.com.tr>
2023-04-17 15:38:06 +0300

    Revert "test"
    This reverts commit e417b99e785d7acfb644f0f0daf3c342d7b0856f.

--- order-service/src/main/java/com/fmss/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/fmss/orderservice/controller/OrderController.java
@@ -13,19 +13,22 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 import static com.fmss.orderservice.constants.OrderConstants.*;
 
 @RestController
 @RequestMapping(API_PREFIX + API_VERSION_V1 + API_ORDER)
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
     private final OrderService orderService;
 
     @Operation(summary = "Create order")
     @ApiResponses(value = @ApiResponse(responseCode = "201", description = "Place order", content = @Content(schema = @Schema(implementation = OrderResponseDTO.class), mediaType = "application/json")))
     @PostMapping("/place-order")
-    @ResponseStatus(HttpStatus.OK)
-    public OrderResponseDTO placeOrder(@RequestBody PlaceOrderRequestDTO placeOrderRequestDTO) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public OrderResponseDTO placeOrder(@RequestBody @Valid PlaceOrderRequestDTO placeOrderRequestDTO) {
         return orderService.placeOrder(placeOrderRequestDTO);
     }
 }
