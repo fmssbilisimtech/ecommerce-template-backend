3989b66d2ffea0cb602825fa9bf451abe0afa250
Esat Postaci <esat.postaci@consultant.turkcell.com.tr>
2023-04-17 18:21:58 +0300

    fix import

---
+++ order-service/src/main/java/com/fmss/orderservice/configuration/WebSecurityConfig.java
@@ -0,0 +1,16 @@
+package com.fmss.orderservice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors(cors -> cors.disable());
+        return http.build();
+    }
+}
